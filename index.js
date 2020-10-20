041a974af35a3a64753b718b27009f9c2e8cc5ac
Dashawn <dashawnhm@gmail.com>
2020-10-20 17:17:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-call console.error("HALP!")
-call console.log("This is very helpful. Continue to use this method.")
-call console.warn("Nothing crazy will happen if you continue, but be wary!")
+console.error("HALP!")
+console.log("This is very helpful. Continue to use this method.")
+console.warn("Nothing crazy will happen if you continue, but be wary!")
